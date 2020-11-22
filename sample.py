43bd42beb1c3bd26c949a4c5bb16db247bf049ac
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-11-22 16:28:48 +0000

    Meeting work
    Co-authored-by: Claire Farrell <cfarrellc@gmail.com>

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ remove the first and last 50 words, ust in case we missed some meta-data, and sa
 
 We need to either get documents of the same length only, or normalize for length. I like selecting fixed length sets, as otherwise the bag of words will be far too large.
 
-
+100 words then remove stopwords
 
 '''
 
@@ -16,6 +16,12 @@ book_dir = 'annotated/'
 
 def get_files(dir):
     return glob.glob(os.path.join(book_dir, '*.txt'))
+
+def remove_trailing(file):
+    # removing first & last 50 words
+    pass
+
+
 
 def main():
     file_names = get_files(book_dir)
--- train.py
+++ train.py
@@ -1,83 +1,38 @@
-def main():
-    pass
-
-if __name__ == '__main__':
-    pass
-
-#Code from lectures (reformatted)
-
-#-----------------------------Tokenization
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.tokenize import WhitespaceTokenizer
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-
-nltk.download('punkt')
-tokenizer = CountVectorizer().build_tokenizer()
-print(tokenizer("Here's example text, isn’t it?"))
-print(WhitespaceTokenizer().tokenize("Here's example text, isn’t it?"))
-print(word_tokenize("Here's example text, isn’t it?"))
-print(tokenizer("likes liking liked"))
-print(WhitespaceTokenizer().tokenize("likes liking liked"))
-print(word_tokenize("likes liking liked"))
-stemmer = PorterStemmer()
-tokens = word_tokenize("Here's example text, isn’t it?")
-stems = [stemmer.stem(token)for token in tokens]
-print(stems)
-tokens = word_tokenize("likes liking liked")
-stems = [stemmer.stem(token)for token in tokens]
-print(stems)
-
-#-----------------------------bag of words
-from sklearn.feature_extraction.text import CountVectorizer
-
-docs = ['This is the first document.','This is the second second document.','And the third one.','Is this the first document?']
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(docs)
-print(vectorizer.get_feature_names())
-print(X.toarray())
-
-vectorizer = CountVectorizer(ngram_range=(2, 2))
-X = vectorizer.fit_transform(docs)
-print(vectorizer.get_feature_names())
-print(X.toarray())
-
-#-----------------------------TF-IDF
-from sklearn.feature_extraction.text import CountVectorizer
-
-docs = ['This is the first document.','This is the second second document.','And the third one.','Is this the first document?']
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(docs)
-print(vectorizer.get_feature_names())
-print(X.toarray())
-
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(norm=None)
-X = vectorizer.fit_transform(docs)
-print(vectorizer.get_feature_names())
-print(X.toarray())
-
-#-----------------------------Recommending news articles example
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics.pairwise import cosine_distances
-from sklearn.neighbors import NearestNeighbors
 
-# first 1000 articles from news dataset at https://www.kaggle.com/snapcrack/all−the−news
-text = pd.read_csv('articles1_1000.csv')
-text.head()
-x = text['content']
-vectorizer = TfidfVectorizer(stop_words = 'english', max_df=0.2)
-X = vectorizer.fit_transform(x)
-indices = np.arange(x.size)
-train, test = train_test_split(indices, test_size=0.2)
-nbrs = NearestNeighbors(n_neighbors=3,metric=cosine_distances).fit(X[train])
-test=[test[0]]
-found = nbrs.kneighbors(X[test], return_distance=False)
-test_i=0
-print('text:\n%.300s'%x[test[test_i]])
-for i in found[0]:
-    print('match %d:\n%.300s'%(i,x[train[i]])
+#hyperparameters
+#to tf-idf tokenizer
+min_len = 2
+max_len = 3
+min_df = 0
+max_df= 1
+
+def create_input_array(documents):
+    '''
+    This function creates a bag of words (X / input) matrix to be used in the model based on the given documents
+    '''
+    vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(min_len, max_len), min_df=min_df, max_df=max_df)
+    X = vectorizer.fit_transform(documents)
+    print('bag of words sumary')
+    print(vectorizer.get_feature_names())
+    print(X.toarray())
+    print()
+    return X
+
+def main():
+    #Gather and process data into a form that we can run ML on
+    documents, y = docs = ['This is the first document.','This is the second second document.','And the third one.','Is this the first document?'], [2020, 2020, 2020, 2020] #Call to sampling.py
+    X = create_input_array(documents)
+
+    #Split into train and test segments
+    indices = np.arange(len(X))
+    train, test = train_test_split(indices, test_size=0.2)
+
+    #
+
+if __name__ == '__main__':
+    main()
