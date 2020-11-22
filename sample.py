3534edd715851c484f84b3b9fa85854faca92681
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-11-22 12:21:27 +0000

    length summary

--- sample.py
+++ sample.py
@@ -1,6 +1,17 @@
 import glob
 import os
 
+'''
+An idea:
+
+remove the first and last 50 words, ust in case we missed some meta-data, and sample from the rest
+
+We need to either get documents of the same length only, or normalize for length. I like selecting fixed length sets, as otherwise the bag of words will be far too large.
+
+
+
+'''
+
 book_dir = 'annotated/'
 
 def get_files(dir):
