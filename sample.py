6f4c417edaf376a3097c2a6c3e28e23d5332571f
Your Name <c16333453@mydit.ie>
2020-11-23 23:51:28 +0000

    sampling

--- sample.py
+++ sample.py
@@ -17,14 +17,31 @@ book_dir = 'annotated/'
 def get_files(dir):
     return glob.glob(os.path.join(book_dir, '*.txt'))
 
-def remove_trailing(file):
+def remove_meta(file):
     # removing first & last 50 words
-    pass
+    splitted = file.split(" ", 50)[50]
+    tokenised = splitted.split(" ")
+    ls = ls[:len(ls)-50]
+    return " ".join(ls)
 
+def sample(doc):
+    tokenised = doc.split(" ")
+    first_sample = tokenised[:30]
+    last_sample = tokenised[-30:]
+    middle_sample = tokenised[find_middle(tokenised)-20:find_middle(tokenised)+20]
+    return " ".join(first_sample) + " " + " ".join(last_sample) + " " + " ".join(middle_sample)
 
+def find_middle(tokenised):
+    middle = float(len(tokenised))/2
+    if middle % 2 != 0:
+        return tokenised[int(middle - .5)]
+    else:
+        return (tokenised[int(middle)], tokenised[int(middle-1)])
 
 def main():
     file_names = get_files(book_dir)
+    doc = remove_meta(file_names)
+    return sample(doc)
 
 if __name__ == '__main__':
     main()
