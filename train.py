2fb8bbe334baa086b880753b50e82424543c9f39
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-11-27 22:19:16 +0000

    train fix

--- train.py
+++ train.py
@@ -7,11 +7,17 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import mean_squared_error
 import sample
+from multiprocessing import Pool
+import random
 
 #Global params
 fold = 5
 total_len = 2000
 num_samples = 20
+
+#Data
+documents = None
+y = None
 
 def create_input_array(documents, phrase_len, min_df, max_df):
     '''
@@ -24,8 +30,6 @@ def create_input_array(documents, phrase_len, min_df, max_df):
 def process_data(documents, y, fold, phrase_len, min_df, max_df):
     #Gather and process data into a form that we can run ML on
     X = create_input_array(documents, phrase_len, min_df, max_df)
-
-    #Split into train and test segments
     xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=1 / fold)
     return X, xtrain, xtest, ytrain, ytest
 
@@ -44,31 +48,39 @@ def evaluations(xtrain, xtest, ytrain, ytest, alpha,):
 
     return lin_reg_mse, lasso_mse, ridge_mse, baseline_mse
 
-def main():
-    phrase_len = 1
-    min_df = int(1) #int for absolute counts, float for proportion
-    max_df= float(1.0) #int for absolute counts, float for proportion
-    alpha = .01
+def init_args(local_documents, local_y):
+    global documents, y
+    documents = local_documents
+    y = local_y
 
-    eval_all()
+def main():
+    global documents, y
+    documents, y = sample.run_sample(total_len, num_samples)
+
+    max_dfs = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
+    min_dfs = [0.0, 0.01, 0.1, 0.2]
+    phrase_lens = [1, 3, 5, 7, 9]
+
+    args_lists = []
+    for max_df in max_dfs:
+        for min_df in min_dfs:
+            for phrase_len in phrase_lens:
+                args_lists.append([max_df, min_df, phrase_len])
+    random.shuffle(args_lists) #Balance out work load--larger phrase len => much longer to train
+
+    processes = 8
+    arg_divs = [args_lists[i * len(args_lists) // processes : (i + 1) * len(args_lists) // processes] for i in range(processes)]
+    pool = Pool(processes=processes, initializer=init_args, initargs=[documents, y])
+    result = pool.map(eval_all, arg_divs)
+    pool.close()
+
+    with open('cross-val.csv', 'w') as out:
+        print('max_df,min_df,phrase_len,alpha,lin_reg_mse,lasso_mse,ridge_mse,baseline_mse', file=out)
+        for data_list in result:
+            for line in data_list:
+                print(line, file=out)
 
     # eval_model(phrase_len, min_df, max_df, alpha)
-    
-    '''max_dfs = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
-    eval_max_df(phrase_len, min_df, alpha, max_dfs)
-    print()
-
-    min_dfs = [0.0, 0.01, 0.1, 0.2]
-    eval_min_df(phrase_len, max_df, alpha, min_dfs)
-    print()
-
-    phrase_lens = [1, 3, 5, 7, 9]
-    eval_phrase_len(min_df, max_df, alpha, phrase_lens)
-    print()
-    
-    alphas = [100, 10, 1, .1, .01, .001]
-    eval_alpha(phrase_len, min_df, max_df, alphas)
-    print()'''
 
 def eval_model(phrase_len, min_df, max_df, alpha):
     documents, y = sample.run_sample(total_len, num_samples)
@@ -80,6 +92,28 @@ def eval_model(phrase_len, min_df, max_df, alpha):
     print('lin_reg_mse,lasso_mse,ridge_mse,baseline_mse')
     print(data_str)
 
+def eval_all(text_params):
+    alphas = [100, 10, 1, .1, .01, .001, .0001]
+    data_strs = []
+    i = 0
+    for max_df, min_df, phrase_len in text_params:
+        try: #Somer param combos are not valid and wilol throw an error
+            _, xtrain, xtest, ytrain, ytest = process_data(documents, y, fold, phrase_len, min_df, max_df)
+            for alpha in alphas:
+                print(i)
+                i += 1
+                
+                data = evaluations(xtrain, xtest, ytrain, ytest, alpha)
+                print(max_df)
+                data_str = str(max_df) + ',' + str(min_df) + ',' + str(phrase_len) + ',' + str(alpha) + ','
+                data_str += ','.join(str(x) for x in data) + '\n'
+                data_strs.append(data_str)
+        except:
+                pass
+                print('error')
+    return data_strs
+
+'''
 def eval_all():
     max_dfs = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
     min_dfs = [0.0, 0.01, 0.1, 0.2]
@@ -108,58 +142,7 @@ def eval_all():
     with open('cross-val.csv', 'w') as out:
         print('max_df,min_df,phrase_len,alpha,lin_reg_mse,lasso_mse,ridge_mse,baseline_mse', file=out)
         print(data_str, file=out)
-
-'''def eval_max_df(phrase_len, min_df, alpha, max_dfs):
-    input_datas = []
-    for val in max_dfs:
-        input_datas.append(load_data(fold, phrase_len, min_df, val))
-
-    data_str = ''
-    for i, (X, y, xtrain, xtest, ytrain, ytest) in enumerate(input_datas):
-        data = evaluations(xtrain, xtest, ytrain, ytest, alpha, max_dfs[i])
-        data_str += ','.join(str(x) for x in data) + '\n'
-    print('Data for max DF')
-    print('max_df,lin_reg_mse,lasso_mse,ridge_mse,baseline_mse')
-    print(data_str)
-
-def eval_min_df(phrase_len, max_df, alpha, min_dfs):
-    input_datas = []
-    for val in min_dfs:
-        input_datas.append(load_data(fold, phrase_len, val, max_df))
-
-    data_str = ''
-    for i, (X, y, xtrain, xtest, ytrain, ytest) in enumerate(input_datas):
-        data = evaluations(xtrain, xtest, ytrain, ytest, alpha, min_dfs[i])
-        data_str += ','.join(str(x) for x in data) + '\n'
-    print('Data for min DF')
-    print('min_df,lin_reg_mse,lasso_mse,ridge_mse,baseline_mse')
-    print(data_str)
-
-def eval_phrase_len(min_df, max_df, alpha, phrase_lens):
-    input_datas = []
-    for val in phrase_lens:
-        input_datas.append(load_data(fold, val, min_df, max_df))
-
-    data_str = ''
-    for i, (X, y, xtrain, xtest, ytrain, ytest) in enumerate(input_datas):
-        data = evaluations(xtrain, xtest, ytrain, ytest, alpha, phrase_lens[i])
-        data_str += ','.join(str(x) for x in data) + '\n'
-    print('Data for phrase length')
-    print('phrase_len,lin_reg_mse,lasso_mse,ridge_mse,baseline_mse')
-    print(data_str)
-
-def eval_alpha(phrase_len, min_df, max_df, alphas):
-    input_datas = []
-    for _ in alphas:
-        input_datas.append(load_data(fold, phrase_len, min_df, max_df))
-
-    data_str = ''
-    for i, (X, y, xtrain, xtest, ytrain, ytest) in enumerate(input_datas):
-        data = evaluations(xtrain, xtest, ytrain, ytest, alphas[i], alphas[i])
-        data_str += ','.join(str(x) for x in data) + '\n'
-    print('Data for alpha')
-    print('alpha,lin_reg_mse,lasso_mse,ridge_mse,baseline_mse')
-    print(data_str)'''
+'''
 
 if __name__ == '__main__':
     main()
