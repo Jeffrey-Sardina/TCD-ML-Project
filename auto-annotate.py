e40619bed10bca62da9309ad3a8c303cd46e8478
Your Name <c16333453@mydit.ie>
2020-12-05 18:40:38 +0000

    annotate updates

--- auto-annotate.py
+++ auto-annotate.py
@@ -16,32 +16,39 @@ def is_valid_date(date_str):
 def remove500words(doc, n):
     tokenised = doc.split(" ")
     removed = tokenised[n:]
-    return removed
+    return ' '.join(removed)
 
 def findDates(doc):
-    return re.match('.*([1-3][0-9]{3})', doc)
+    return re.search('(1[6-9]{3})', doc).group()
 
 def get_files(dir):
     return glob.glob(os.path.join(book_dir, '*.txt'))
 
 def main():
     book_file_names = get_files(book_dir)
+    count=0
     # print(book_file_names)
+    
     for book_file_name in book_file_names:
+        count+=1
         with open(book_file_name, 'r') as inp:
-            doc = inp.readlines()
-            year = findDates(doc)
-            directory = 'auto_annotated'
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            dpath = '%s/%s.txt' % (directory, year)
-            uniq = 1
-            while os.path.exists(dpath):
-                dpath = '%s/%s_%d.txt' % (directory, year, uniq)
-                uniq += 1
-            # with open(dpath, 'w') as out:
-            print(dpath)
-
+            try:
+                doc = inp.read()
+                year = findDates(doc)
+                directory = 'auto_annotated'
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                dpath = '%s/%s.txt' % (directory, year)
+                uniq = 1
+                while os.path.exists(dpath):
+                    dpath = '%s/%s_%d.txt' % (directory, year, uniq)
+                    uniq += 1
+                with open(dpath, 'w') as out:
+                    doc1 = remove500words(doc, 500)
+                    out.write(doc1)
+            except:
+                print('nah')
+    print(count)
 
 if __name__ == '__main__':
     main()
