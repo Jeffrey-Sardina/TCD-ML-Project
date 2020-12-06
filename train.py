54b0c189c2ba49fee1de9b485478017e58269723
Jeffrey (Siothrún) Sardina <jeffrey.sardina@gmail.com>
2020-12-06 12:12:29 +0000

    added in training for KNN

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.dummy import DummyRegressor
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
@@ -58,6 +59,20 @@ def evaluations(xtrain, xtest, ytrain, ytest, alpha):
     baseline_mse = mean_squared_error(ytest, baseline.predict(xtest))
 
     return lin_reg_mse, lasso_mse, ridge_mse, baseline_mse
+
+def evaluations_knn(xtrain, xtest, ytrain, ytest, k):
+    '''
+    This code runs evaluations of various linear regression models against a baseline. The alpha parameter is the multiplier to the regularization penalty in Lasso and Ridge Regression.
+    '''
+    #Create models
+    knn_model = KNeighborsRegressor(n_neighbors=k).fit(xtrain, ytrain)
+    baseline = DummyRegressor(strategy="mean").fit(xtrain, ytrain)
+
+    #Evaluate models
+    knn_mse = mean_squared_error(ytest, knn_model.predict(xtest))
+    baseline_mse = mean_squared_error(ytest, baseline.predict(xtest))
+
+    return knn_mse, baseline_mse
 
 ###########################################################
 # Post-cross-validation model evaludation code
@@ -143,6 +158,36 @@ def cross_validations(out_file_name):
             for line in data_list:
                 print(line, file=out)
 
+def cross_validations_knn(out_file_name):
+    '''
+    This function runs corss-validations for all valid combinations of a range of hyperparameters for a KNN regressor. Code is multiprocessed to speed it up, and results are written to a CSV file.
+    '''
+    global documents, y
+    documents, y = sample.run_sample(total_len, num_samples)
+
+    max_dfs = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
+    min_dfs = [0.0, 0.01, 0.1, 0.2]
+    phrase_lens = [1, 3, 5, 7, 9]
+
+    args_lists = []
+    for max_df in max_dfs:
+        for min_df in min_dfs:
+            for phrase_len in phrase_lens:
+                args_lists.append([max_df, min_df, phrase_len])
+    random.shuffle(args_lists) #Balance out work load--larger phrase len => much longer to train
+
+    processes = 8
+    arg_divs = [args_lists[i * len(args_lists) // processes : (i + 1) * len(args_lists) // processes] for i in range(processes)]
+    pool = Pool(processes=processes, initializer=init_args, initargs=[documents, y])
+    result = pool.map(eval_all_knn, arg_divs)
+    pool.close()
+
+    with open(out_file_name, 'w') as out:
+        print('max_df,min_df,phrase_len,k,knn_mse,baseline_mse', file=out)
+        for data_list in result:
+            for line in data_list:
+                print(line, file=out)
+
 def eval_all(text_params):
     '''
     This function does the 'brunt work' of cross-validation--given a list of max_df, min_df, and phrase_len tuples (text_params) as hyperparameters and using a range of alpha (regularization multiplier) values, it runs evaluations on all of them and returns the MSEs of the results.
@@ -166,6 +211,26 @@ def eval_all(text_params):
                 print('error')
     return data_strs
 
+def eval_all_knn(text_params):
+    ks = [1, 5, 25, 50, 100, 150, 200, 300]
+    data_strs = []
+    i = 0
+    for max_df, min_df, phrase_len in text_params:
+        try: #Somer param combos are not valid and wilol throw an error
+            _, xtrain, xtest, ytrain, ytest = process_data(documents, y, fold, phrase_len, min_df, max_df)
+            for k in ks:
+                print(i)
+                i += 1
+                
+                data = evaluations_knn(xtrain, xtest, ytrain, ytest, k)
+                data_str = str(max_df) + ',' + str(min_df) + ',' + str(phrase_len) + ',' + str(k) + ','
+                data_str += ','.join(str(x) for x in data)
+                data_strs.append(data_str)
+        except:
+                pass
+                print('error')
+    return data_strs
+
 def init_args(local_documents, local_y):
     '''
     This function is used to provide global parameters to each process when multiprocessing
@@ -179,10 +244,15 @@ def init_args(local_documents, local_y):
 ###########################################################
 
 def main():
-    #Run cross-validation several times to get enough data to calc standard deviations
+    #Run cross-validation several times to get enough data to calc standard deviations (for linear regression variants)
     '''n = 100
     for i in range(n):
         cross_validations('cross-val2_' + str(i) + '.csv')'''
+
+    #Run cross-validation several times to get enough data to calc standard deviations (for kNN)
+    n = 1
+    for i in range(n):
+        cross_validations_knn('cross-val_knn' + str(i) + '.csv')
 
     #Construct a model based on the optimal hyperparameters and model type
     '''max_df = 0.3
@@ -207,14 +277,14 @@ def main():
         print(np.std(diffs, ddof=1))
         print()'''
 
-    #Final model
-    max_df = 0.3
+    #Best of the linear models
+    '''max_df = 0.3
     min_df = 0.01
     phrase_len = 3
     alpha = 0.1
     model = 'Ridge'
     random_seed = 42
-    eval_model_advanced(phrase_len, min_df, max_df, alpha, model, random_seed)
+    eval_model_advanced(phrase_len, min_df, max_df, alpha, model, random_seed)'''
 
 if __name__ == '__main__':
     main()
